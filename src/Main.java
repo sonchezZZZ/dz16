b16e90aad1d1a2ac02a0c4f613140f5eab740d4e
Sofiia Cherednychenko <ruslanovna1604@gmail.com>
2021-07-31 16:35:07 +0300

    /new methods for dz 16

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class Main {
+    public static final void main(String[] args) {
+        System.out.println(compareDogAndCat("1catdog"));
+//        System.out.println(returnMap("salt", "tea", "soda", "toast"));
+        System.out.println(returnWithoutSubstring("Helloxxx there", "xx"));
+
+
+    }
+
+    //  method for first task
+    public static boolean compareDogAndCat(String line) {
+        boolean check;
+        if (line.contains("cat") && line.contains("dog")) {
+            check = true;
+            while (line.contains("cat") && line.contains("dog")) {
+                line = line.replaceFirst("cat", "");
+                line = line.replaceFirst("dog", "");
+            }
+            if (line.contains("cat") || line.contains("dog")) {
+                check = false;
+            }
+        } else {
+            check = false;
+        }
+        return check;
+    }
+
+    //  method for second task
+    public static Map<Character, String> returnMap(String... mass) {
+        Map<Character, String> map = new HashMap<>();
+        for (int i = 0; i < mass.length; i++) {
+            char key = mass[i].charAt(0);
+            if (map.containsKey(key)) {
+                map.replace(key, map.get(key) + mass[i]);
+            } else {
+                map.put(key, mass[i]);
+            }
+        }
+        return map;
+    }
+
+    //    method for third task
+    public static String returnWithoutSubstring(String base, String removeLine) {
+        boolean trying = true;
+        while (trying) {
+            base = base.replaceFirst(removeLine, "");
+            if (!base.contains(removeLine)) {
+                trying = false;
+            }
+        }
+        return base;
+    }
+}
